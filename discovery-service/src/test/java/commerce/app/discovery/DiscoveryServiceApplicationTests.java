f1657fbe2eabe3eff9e081d796d0278f2bda7d75
Mark <m.gurianov@mail.ru>
2022-01-05 00:55:32 +0300

    add discovery-service

---
+++ discovery-service/src/test/java/commerce/app/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package commerce.app.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
